18d2e22842426661ab2723b4d1f41b2d1bd19c32
dmwilliamstech <dmwilliams.tech@gmail.com>
2015-07-07 09:56:06 -0400

    moved to var

--- gitlab-installation.sh
+++ gitlab-installation.sh
@@ -59,10 +59,11 @@ fi
 
 echo "Configure Gitlab to run on desired IP and Port"
 GITLAB_CONFIG=/etc/gitlab/gitlab.rb
+EXTERNAL_URL=$git_ip\:$git_port
 if [ -f $GITLAB_CONFIG ];
 then
   echo "Configuring GitLab URL and Port"
-  sed -i "s/^external_url .*$/external_url \"http://${git_ip/:}${git_port}\"/" $GITLAB_CONFIG
+  sed -i "s/^external_url .*$/external_url \"http://${EXTERNAL_URL}\"/" $GITLAB_CONFIG
 else
   echo "This '$GITLAB_CONFIG' is missing"
 fi
