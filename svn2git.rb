20a73d422c8f49d2fee8fe719fd9232a12291414
dmwilliamstech <dmwilliams.tech@gmail.com>
2015-07-08 15:27:19 -0400

    removed the creation of the users.txt in .rb

--- svn2git.rb
+++ svn2git.rb
@@ -3,10 +3,13 @@
 @repos = { "repo1" => 82, "repo2" => 94, "repo3" => 58 }
 
 ## now loop through the above array
-echo """svn log -q | awk -F \'|\' \'/^r/ {sub("^ ", "", $2); sub(" \$", "", \$2)\; print \$2" = "\$2" <"\$2">"}' | sort -u > users.txt"""
 
 @repos.each do|name,location|
   puts "#{name}: #{location}"
   puts "Cloning repo #{name}"
-  system "git svn clone $i --authors-file=users.txt --no-metadata -s ."
+	  if File.exist?('users.txt')
+	  system "git svn clone $i --authors-file=users.txt --no-metadata -s ."
+	else
+		puts "No users.txt present"
+	end
 end
