38cb861457a6fe40a3ff2e8c75a94d80b94de4df
dmwilliamstech <dmwilliams.tech@gmail.com>
2015-07-08 15:08:25 -0400

    created svn2git ruby file

---
+++ svn2git.rb
@@ -0,0 +1,12 @@
+
+## @repos is a map of repo names and locations
+@repos = { "repo1" => 82, "repo2" => 94, "repo3" => 58 }
+
+## now loop through the above array
+echo """svn log -q | awk -F '|' '/^r/ {sub("^ ", "", $2); sub(" $", "", $2); print $2" = "$2" <"$2">"}' | sort -u > users.txt"""
+
+@repos.each do|name,location|
+  puts "#{name}: #{location}"
+  puts "Cloning repo #{name}"
+  system "git svn clone $i --authors-file=users.txt --no-metadata -s ."
+end
