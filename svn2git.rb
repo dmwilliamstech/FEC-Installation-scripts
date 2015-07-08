6f090bd27767006bccae30d64a0c7ca06328dd64
dmwilliamstech <dmwilliams.tech@gmail.com>
2015-07-08 15:38:48 -0400

    moved conditional

--- svn2git.rb
+++ svn2git.rb
@@ -5,9 +5,9 @@
 ## now loop through the above array
 
 @repos.each do|name,location|
-  puts "#{name}: #{location}"
-  puts "Cloning repo #{name}"
-	  if File.exist?('users.txt')
+	if File.exist?('users.txt')
+	  puts "#{name}: #{location}"
+	  puts "Cloning repo #{name}"
 	  system "git svn clone $i --authors-file=users.txt --no-metadata -s ."
 	  system "cp -Rf .git/refs/remotes/origin/tags/* .git/refs/tags/"
 	  system "rm -Rf .git/refs/remotes/origin/tags"
@@ -15,7 +15,6 @@
 	  system "rm -Rf .git/refs/remotes"
 	  system "git remote add master git@localhost:gitlab-users/#{name}.git"
 	  system "git push --set-upstream gitlab master"
-
 	else
 		puts "No users.txt present"
 	end
