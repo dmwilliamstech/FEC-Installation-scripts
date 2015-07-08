5ec4007af77a27a0b8071a4739cd9597130f24fc
dmwilliamstech <dmwilliams.tech@gmail.com>
2015-07-08 15:19:27 -0400

    tried to escape

--- svn2git.rb
+++ svn2git.rb
@@ -3,7 +3,7 @@
 @repos = { "repo1" => 82, "repo2" => 94, "repo3" => 58 }
 
 ## now loop through the above array
-echo """svn log -q | awk -F '|' '/^r/ {sub("^ ", "", $2); sub(" $", "", $2); print $2" = "$2" <"$2">"}' | sort -u > users.txt"""
+echo """svn log -q | awk -F \'|\' \'/^r/ {sub("^ ", "", $2); sub(" \$", "", \$2)\; print \$2" = "\$2" <"\$2">"}' | sort -u > users.txt"""
 
 @repos.each do|name,location|
   puts "#{name}: #{location}"
